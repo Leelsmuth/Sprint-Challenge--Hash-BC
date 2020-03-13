d13a1d5963bbce5d935f9450fa66938b884a380d
Leelsmuth <femianjorin@rocketmail.com>
2020-03-13 12:16:51 +0100

    added comments to code

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -14,14 +14,15 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     
     for i in range(length):
-      # finding the limit
-      pair_turple = hash_table_retrieve(ht, limit - weights[i])
-      if pair_turple is not None:
-        print(i, pair_turple)
-        return (i, pair_turple)
-      
-      else:
-          hash_table_insert(ht, weights[i], i)
+        # find match based on current index locations value
+        difference = limit - weights[i]
+        # get current value from hashtable
+        match = hash_table_retrieve(ht, difference)
+        if match is not None:
+            # if match found return with current index first
+            return [i, match]
+        # else no match build the hashtable 
+        hash_table_insert(ht, weights[i], i)
 
     return None
 
